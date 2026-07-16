c0e3ad9c7a580f7ce82b05d048c7e4105cb57024
Aleksandr Beliakov <aleksandr.beliakov@nowina.lu>
2026-07-16 12:47:59 +0200

    DSS-3925 : addMdocDeviceResponseEAAPresentationValidator test

--- dss-eaa-mdoc/src/test/java/eu/europa/esig/dss/eaa/mdoc/validation/MdocDeviceResponseEAAPresentationValidatorTest.java
+++ dss-eaa-mdoc/src/test/java/eu/europa/esig/dss/eaa/mdoc/validation/MdocDeviceResponseEAAPresentationValidatorTest.java
@@ -1,8 +1,10 @@
 package eu.europa.esig.dss.eaa.mdoc.validation;
 
+import co.nstant.in.cbor.model.UnicodeString;
 import eu.europa.esig.dss.cbades.cbor.CBORArray;
 import eu.europa.esig.dss.cbades.cbor.CBORByteString;
 import eu.europa.esig.dss.cbades.cbor.CBORMap;
+import eu.europa.esig.dss.cbades.cbor.CBORSimpleObject;
 import eu.europa.esig.dss.cbades.cbor.CBORUtils;
 import eu.europa.esig.dss.eaa.common.validation.DefaultEAAPresentationValidator;
 import eu.europa.esig.dss.model.DSSDocument;
@@ -24,14 +26,36 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class MdocIssuerSignedEAAPresentationValidatorTest extends AbstractTestDocumentValidator {
+class MdocDeviceResponseEAAPresentationValidatorTest extends AbstractTestDocumentValidator {
 
-    private static final DSSDocument MINIMAL_ISSUER_SIGNED = buildMinimalIssuerSigned();
+    private static final DSSDocument MINIMAL_DEVICE_RESPONSE = buildMinimalDeviceResponse();
 
-    private static DSSDocument buildMinimalIssuerSigned() {
+    private static DSSDocument buildMinimalDeviceResponse() {
+        CBORMap issuerSigned = new CBORMap();
+        issuerSigned.put("version", new CBORSimpleObject(new UnicodeString("1.0")));
+        issuerSigned.put("status", 0);
+        return new InMemoryDocument(CBORUtils.serializeCborObject(issuerSigned));
+    }
+
+    private static CBORMap buildDocument() {
+        CBORMap document = new CBORMap();
+        document.put("docType", "org.iso.18013.5.1.mDL");
+        document.put("issuerSigned", buildIssuerSigned());
+        document.put("deviceSigned", buildDeviceSigned());
+        return document;
+    }
+
+    private static CBORMap buildIssuerSigned() {
         CBORMap issuerSigned = new CBORMap();
         issuerSigned.put("issuerAuth", buildCoseSign1());
-        return new InMemoryDocument(CBORUtils.serializeCborObject(issuerSigned));
+        return issuerSigned;
+    }
+
+    private static CBORMap buildDeviceSigned() {
+        CBORMap deviceSigned = new CBORMap();
+        deviceSigned.put("nameSpaces", new CBORMap().getByteString());
+        deviceSigned.put("deviceAuth", buildCoseSign1());
+        return deviceSigned;
     }
 
     private static CBORArray buildCoseSign1() {
@@ -50,32 +74,35 @@ class MdocIssuerSignedEAAPresentationValidatorTest extends AbstractTestDocumentV
         return new CBORByteString(CBORUtils.serializeCborObject(msoBytes));
     }
 
+    private static CBORMap buildDocumentError() {
+        CBORMap document = new CBORMap();
+        document.put("org.iso.23220.1.mID", 1);
+        return document;
+    }
+
     @Test
     void test() {
-        MdocIssuerSignedEAAPresentationValidator validator = new MdocIssuerSignedEAAPresentationValidator();
+        MdocDeviceResponseEAAPresentationValidator validator = new MdocDeviceResponseEAAPresentationValidator();
 
-        DSSDocument mdoc = MINIMAL_ISSUER_SIGNED;
+        DSSDocument mdoc = MINIMAL_DEVICE_RESPONSE;
         assertTrue(validator.isSupported(mdoc));
 
-        CBORMap nameSpacesOnly = new CBORMap();
-        nameSpacesOnly.put("nameSpaces", new CBORMap());
-        mdoc = new InMemoryDocument(CBORUtils.serializeCborObject(nameSpacesOnly));
+        CBORMap allKeys = new CBORMap();
+        allKeys.put("version", new CBORSimpleObject(new UnicodeString("1.0")));
+        allKeys.put("documents", new CBORArray(Collections.singletonList(buildDocument())));
+        allKeys.put("documentErrors", new CBORArray(Collections.singletonList(buildDocumentError())));
+        allKeys.put("status", 0);
+        mdoc = new InMemoryDocument(CBORUtils.serializeCborObject(allKeys));
         assertTrue(validator.isSupported(mdoc));
 
-        CBORMap bothKeys = new CBORMap();
-        bothKeys.put("nameSpaces", new CBORMap());
-        bothKeys.put("issuerAuth", buildCoseSign1());
-        mdoc = new InMemoryDocument(CBORUtils.serializeCborObject(bothKeys));
-        assertTrue(validator.isSupported(mdoc));
-
-        mdoc = new FileDocument("src/test/resources/validation/mdocIssuerSigned.cbor");
+        mdoc = new FileDocument("src/test/resources/validation/mdocRefImpl.mdoc");
         assertTrue(validator.isSupported(mdoc));
 
         CBORMap wrongKeyMap = new CBORMap();
         wrongKeyMap.put("test", 1);
         DSSDocument wrong = new InMemoryDocument(CBORUtils.serializeCborObject(wrongKeyMap));
         assertFalse(validator.isSupported(wrong));
-        wrong = new FileDocument("src/test/resources/validation/mdocRefImpl.mdoc");
+        wrong = new FileDocument("src/test/resources/validation/mdocIssuerSigned.cbor");
         assertFalse(validator.isSupported(wrong));
         wrong = new InMemoryDocument(CBORUtils.serializeCborObject(new CBORArray()));
         assertFalse(validator.isSupported(wrong));
@@ -93,17 +120,17 @@ class MdocIssuerSignedEAAPresentationValidatorTest extends AbstractTestDocumentV
 
     @Override
     protected SignedDocumentValidator initEmptyValidator() {
-        return new MdocIssuerSignedEAAPresentationValidator();
+        return new MdocDeviceResponseEAAPresentationValidator();
     }
 
     @Override
     protected SignedDocumentValidator initValidator(DSSDocument document) {
-        return new MdocIssuerSignedEAAPresentationValidator(document);
+        return new MdocDeviceResponseEAAPresentationValidator(document);
     }
 
     @Override
     protected List<DSSDocument> getValidDocuments() {
-        return Collections.singletonList(new FileDocument("src/test/resources/validation/mdocIssuerSigned.cbor"));
+        return Collections.singletonList(new FileDocument("src/test/resources/validation/mdocRefImpl.mdoc"));
     }
 
     @Override
@@ -113,13 +140,12 @@ class MdocIssuerSignedEAAPresentationValidatorTest extends AbstractTestDocumentV
 
     @Override
     protected DSSDocument getOtherTypeDocument() {
-        return new FileDocument("src/test/resources/validation/mdocRefImpl.mdoc");
+        return new FileDocument("src/test/resources/validation/mdocIssuerSigned.cbor");
     }
 
     @Override
     protected DSSDocument getNoSignatureDocument() {
-        // not applicable
-        return null;
+        return MINIMAL_DEVICE_RESPONSE;
     }
 
     @Override
